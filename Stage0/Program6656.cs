5d326caa0f0d13429a5362d800e3d432bf71d38f
Rlwolfe <rlwolfe94@gmail.com>
2022-11-09 17:18:58 +0200

    Our first main

---
+++ Stage0/Program6656.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Stage0
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Welcome6656(); Console.ReadKey();
+		}
+
+		private static void Welcome6656()
+		{
+			Console.WriteLine("Enter your name: ");
+			string? name = Console.ReadLine();
+			Console.WriteLine("{0}, welcome to my first console application", name);  //($"{name}, welcome to my first console application");
+		}
+	}
+}
