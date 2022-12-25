2807c4e8813fdd16b604d26fcfdf746a568df7a8
Rlwolfe <rlwolfe94@gmail.com>
2022-12-25 20:41:11 +0200

    starting BlTest

---
+++ BlTest/BlTest.cs
@@ -0,0 +1,13 @@
+﻿using BlApi;
+using BlImplementation;
+using Dal;
+namespace BlTest;
+class BlTest
+{
+    static IBl bl = new Bl;
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Hello, World!");
+    }
+}
--- BlTest/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlTest;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
